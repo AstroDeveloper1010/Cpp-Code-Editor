db261e281e609c52a6572e430e55f39ab513eebe
Priyanshu Jha <priyanshujha131@gmail.com>
2023-08-25 13:07:56 +0530

    fixed download code feature

--- public/js/script.js
+++ public/js/script.js
@@ -1,107 +1,36 @@
 // declare variables
-const editorContainer = document.querySelector(".editor-container");
-const runBtn = document.querySelector('.btn_run');
-const clearBtn = document.querySelector('.btn_clear');
-const downloadBtn = document.querySelector('.btn_download');
 const codeInput = document.querySelector('.code-input');
-const outputArea = document.querySelector('.ouput');
-const lineNumberDiv = document.querySelector(".line-number");
-const toggleButton = document.querySelector(".btn_toggle");
+const lineNumber = document.querySelector('.line-number');
+const downloadBtn = document.querySelector('.btn_download');
 
 
-// on clicking run btn
-runBtn.addEventListener('click', () => {
-    const code = codeInput.value;
-
-    fetch('/run', {
-        method: POST,
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ code })
-    })
-        .then(response => response.json())
-        .then(data => {
-            outputArea.textContent = data.output;
-        })
-        .catch(error => {
-            console.error(`Error: ${error}`);
-            outputArea.textContent = 'Output will appear here...';
-        });
-});
-
-
-// on clicking clear btn
-clearBtn.addEventListener('click', () => {
-    codeInput.value = '';
-});
-
-
-// updating line number
-codeInput.addEventListener('click', (updateLineNumber));
-
-const updateLineNumber = () => {
-    const line = codeInput.value.split("\n");
-    const lineNumberHTML = line.map((_, index) => `<span>${index + 1}</span>`).join("");
-    lineNumberDiv.innerHTML = lineNumberHTML;
+// Function to update line numbers
+const updateLineNumbers = () => {
+    const lines = codeInput.value.split('\n');
+    const lineNumbersHTML = lines.map((_, index) => `<div>${index + 1}</div>`).join('');
+    lineNumber.innerHTML = lineNumbersHTML;
 }
 
-updateLineNumber();
+// event listeners
+codeInput.addEventListener('input', updateLineNumbers);
+window.addEventListener('load', updateLineNumbers);
 
 
-// function to change theme of the code editor
-toggleButton.addEventListener("click", toggleTheme);
+// Function to handle download
+function downloadCode() {
+    const codeContent = codeInput.value;
+    const blob = new Blob([codeContent], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
 
-const toggleTheme = () => {
-    editorContainer.classList.toggle("dark-theme");
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'code.cpp'; // Change the filename as needed
+    a.style.display = 'none';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
 }
 
-
-// donwloading code written by the user
-downloadBtn.addEventListener("click", downloadCode);
-
-const downloadCode = () => {
-    const code = codeInput.value;
-    const blob = new Blob([code], { type: "text/plain" });
-
-    const link = document.createElement("a");
-    link.href = URL.createObjectURL(blob);
-    link.download = "my_code.cpp";
-    link.click();
-
-    URL.revokeObjectURL(link.href);
-}
-
-
-// auto complete and intendation function
-codeInput.addEventListener('input', autoComplete);
-
-const autoComplete = () => {
-    const indentSize = 4;
-    const lines = codeInput.value.split("\n");
-
-    for (let i = 1; i < lines.length; i++) {
-        if (lines[i - 1].trim().endsWith("{")) {
-            lines[i] = " ".repeat(indentSize) + lines[i];
-        }
-    }
-
-    codeInput.value = lines.join("\n");
-}
-
-
-// tab btn space feature
-codeInput.addEventListener("keydown", tabBtnSpace);
-
-const tabBtnSpace = () => {
-    if (e.key === "Tab") {
-        e.preventDefault();
-
-        const start = codeInput.selectionStart;
-        const end = codeInput.selectionEnd;
-
-        codeInput.value = codeInput.value.substring(0, start) + "    " + codeInput.value.substring(end);
-
-        codeInput.selectionStart = codeInput.selectionEnd = start + 4;
-    }
-}
+// Attach event listener to the button
+downloadBtn.addEventListener('click', downloadCode);
--- server.js
+++ server.js
@@ -1,35 +1,54 @@
+// importing the libraries
 const express = require('express');
 const path = require('path');
-const { exec } = require('child_process');
-const { stderr } = require('process');
+const bodyParser = require('body-parser');
+// const cpp = require('./lib/cpp.js');
+// const iL = require('./lib/interpretedLang.js');
+// const { createUnique } = require('./lib/randomString.js');
+const cors = require('cors');
 
+// initialising our express app
 const app = express();
 
+// setting the view engine ejs
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname));
 
+// storing the directory path in pathfile
 const pathfile = path.join(__dirname, 'public');
 
+// setting our app to use static files (css and js)
 app.use(express.static(path.join(pathfile)));
 app.use(express.json());
+app.use(express.urlencoded());
 
+// requesting index.ejs at the home directory
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.post('/run', (req, res) => {
+// running cpp code
+app.post('/run', async (req, res) => {
     const code = req.body.code;
+    const input = req.body.input;
 
-    exec('g++ -o output code.cpp && ./output', (error, stdout, stderr) => {
-        if (error) {
-            res.status(500).json({ error: stderr });
-        }
-        else {
-            res.status(200).json({ output: stdout });
-        }
-    });
+    try {
+        const name = createUnique();
+        const compile = await cpp.writeCode(code);
+        const execCode = await compile(name);
+        const data = await execCode(name, input);
+
+        res.send({
+            'error': null,
+            'output': data
+        })
+    }
+    catch (e) {
+        res.send({ 'error': e.toString() });
+    }
 });
 
+// listening at port 4000
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
